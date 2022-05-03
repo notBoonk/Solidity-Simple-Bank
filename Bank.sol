c605cea95e1f3b18eb05120ec7b0176c13fd3b98
notBoonk <103270313+notBoonk@users.noreply.github.com>
2022-05-03 14:45:50 -0400

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.0;
 
 contract Bank { 
 
-    mapping(address => uint256) public AccountBalances;
+    mapping(address => uint256) private AccountBalances;
+
+    function balance() external view returns(uint256) {
+        return AccountBalances[msg.sender];
+    }
 
     function deposit() external payable {
         AccountBalances[msg.sender] += msg.value;
@@ -16,10 +20,6 @@ contract Bank {
 
         AccountBalances[msg.sender] = 0;
         payable(msg.sender).transfer(currentBalance);
-    }
-
-    function balance() external view returns(uint256) {
-        return AccountBalances[msg.sender];
     }
 
 }
